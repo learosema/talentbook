e83045a36fa47c38ac2d268da59eff6942b2ff8f
Lea Rosema <terabaud@gmail.com>
2020-02-10 09:19:23 +0100

    chore: mark it TODO

--- frontend/src/components/text-input/text-input.tsx
+++ frontend/src/components/text-input/text-input.tsx
@@ -13,6 +13,7 @@ type TextInputProps = {
   readOnly ?: boolean;
 }
 
+// TODO: https://reactjs.org/docs/forwarding-refs.html
 export const TextInput: React.FC<TextInputProps> = ({name, id, type = 'text', value, onChange, className = '', disabled = false, readOnly = false, ref = null}) => (
   <input className={'text-input ' + className} 
     name={name} ref={ref} id={id} type={type} onChange={onChange} value={value} disabled={disabled} readOnly={readOnly} />
