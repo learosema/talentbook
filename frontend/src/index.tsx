1a7f33fad7a5f8090405da9b22f24dc7e8a9522f
Lea Rosema <terabaud@gmail.com>
2020-02-24 22:52:07 +0100

    feat: cross browser compat

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './css/index.scss';
 
+import './polyfills';
 import App from './components/app';
 import * as serviceWorker from './serviceWorker';
 ReactDOM.render(<App />, document.getElementById('root'));
