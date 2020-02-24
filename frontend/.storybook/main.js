7bfc8a9575bd0580924c678659df1b86ca8b3129
Lea Rosema <terabaud@gmail.com>
2020-02-24 17:32:22 +0100

    feat: reconfigure storybook for react

--- frontend/.storybook/main.js
+++ frontend/.storybook/main.js
@@ -1,8 +1,38 @@
+const path = require('path');
+
 module.exports = {
   stories: ['../src/**/*.stories.tsx'],
   addons: [
-    '@storybook/preset-create-react-app',
     '@storybook/addon-actions',
     '@storybook/addon-links',
+    '@storybook/preset-typescript'
   ],
+  webpackFinal: async (config, { configType }) => {
+    // `configType` has a value of 'DEVELOPMENT' or 'PRODUCTION'
+    // You can change the configuration based on that.
+    // 'PRODUCTION' is used when building the static version of storybook.
+
+    config.module.rules.push({
+      test: /\.scss$/,
+      use: ['style-loader', 'css-loader', 'sass-loader'],
+      include: path.resolve(__dirname, '../')
+    });
+
+    config.module.rules.push({
+      test: /\.(ts|tsx)$/,
+      use: [
+        {
+          loader: require.resolve('ts-loader')
+        },
+        // Optional
+        {
+          loader: require.resolve('react-docgen-typescript-loader')
+        }
+      ]
+    });
+    config.resolve.extensions.push('.ts', '.tsx');
+
+    // Return the altered config
+    return config;
+  }
 };
