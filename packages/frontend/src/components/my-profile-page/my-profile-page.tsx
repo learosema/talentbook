0d6d9ac4a5790fabbbe3a1b74c3f76fd337c66a8
Lea Rosema <lea@lea.codes>
2022-03-19 11:03:10 +0100

    chore: make my profile page work with react-query

--- packages/frontend/src/components/my-profile-page/my-profile-page.tsx
+++ packages/frontend/src/components/my-profile-page/my-profile-page.tsx
@@ -1,6 +1,6 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { SkillApi, User } from '../../client/skill-api';
-import { ErrorList } from '../error-list/error-list';
+import { ErrorItem, ErrorList } from '../error-list/error-list';
 import { sendToast } from '../toaster/toaster';
 import { Button, ButtonType, ButtonKind } from '../button/button';
 import { FieldSet } from '../field-set/field-set';
@@ -10,19 +10,74 @@ import { TextArea } from '../text-area/text-area';
 import { Actions } from '../../store/app.actions';
 import { useAppStore } from '../../store/app.context';
 import { ApiException } from '../../client/ajax';
+import {
+  useMutation,
+  useQuery,
+  useQueryClient,
+  UseQueryResult,
+} from 'react-query';
+import { useNavigate } from 'react-router-dom';
 
 export const MyProfilePage: React.FC = () => {
   const { state, dispatch } = useAppStore();
-  const { myProfile, identity } = state;
-  const { userData, errors } = myProfile;
+  const { identity } = state;
+  const navigate = useNavigate();
+  const [userData, setUserData] = useState<User | null>(null);
+  const [errors, setErrors] = useState<ErrorItem[] | null>(null);
+  const queryClient = useQueryClient();
+  const userQuery: UseQueryResult<User> = useQuery(
+    ['user', identity?.name],
+    () => SkillApi.getUser(identity?.name || '').send()
+  );
 
   useEffect(() => {
-    const req = SkillApi.getUser(identity?.name || '');
-    if (identity && identity.name) {
-      req.send().then((data) => dispatch(Actions.setUserData(data)));
+    if (typeof userQuery.data !== 'undefined') {
+      setUserData(userQuery.data);
     }
-    return () => req.abort();
-  }, [identity, dispatch]);
+  }, [userQuery.data]);
+
+  const updateUserMutation = useMutation(
+    async () => {
+      if (!identity || !identity.name || !userData) {
+        throw new Error('unauthenticated');
+      }
+      try {
+        await SkillApi.updateUser(identity.name, userData!).send();
+        if (
+          userData?.name !== identity.name ||
+          userData?.fullName !== identity.fullName
+        ) {
+          // If the username gets changed, then the authentication cookie is renewed.
+          const newIdentity = await SkillApi.getLoginStatus().send();
+          dispatch(Actions.setIdentity(newIdentity));
+        }
+      } catch (ex: any) {
+        if (ex.details && ex.details.details instanceof Array) {
+          setErrors(ex.details.details);
+        }
+        if (ex.name === 'ApiException') {
+          sendToast((ex as ApiException).message);
+        }
+      }
+    },
+    {
+      onSuccess: () => {
+        queryClient.invalidateQueries(['user', userData?.name]);
+      },
+    }
+  );
+
+  const deleteUserMutation = useMutation(
+    async () => {
+      if (!identity || !identity.name) {
+        throw new Error('unauthenticated');
+      }
+      await SkillApi.deleteUser(identity.name).send();
+    },
+    {
+      onSuccess: () => dispatch(Actions.setIdentity(null)),
+    }
+  );
 
   const logoutHandler = async (e: React.MouseEvent) => {
     e.preventDefault();
@@ -38,18 +93,10 @@ export const MyProfilePage: React.FC = () => {
     e.preventDefault();
     try {
       if (!identity || !userData) {
-        return;
+        throw new Error('unauthenticated');
       }
-      await SkillApi.updateUser(identity?.name, userData).send();
-      if (
-        userData.name !== identity.name ||
-        userData.fullName !== identity.fullName
-      ) {
-        // If the username gets changed, then the authentication cookie is renewed.
+      await updateUserMutation.mutateAsync();
 
-        const newIdentity = await SkillApi.getLoginStatus().send();
-        dispatch(Actions.setIdentity(newIdentity));
-      }
       sendToast('saved.');
     } catch (ex: any) {
       console.error(ex);
@@ -58,7 +105,7 @@ export const MyProfilePage: React.FC = () => {
         ex.details &&
         ex.details.details instanceof Array
       ) {
-        dispatch(Actions.setMyProfileErrors(ex.details.details));
+        setErrors(ex.details.details);
       }
     }
   };
@@ -71,8 +118,8 @@ export const MyProfilePage: React.FC = () => {
     ) {
       return;
     }
-    await SkillApi.deleteUser(identity.name).send();
-    document.location.href = '/';
+    await deleteUserMutation.mutateAsync();
+    navigate('/');
   };
 
   const downloadHandler = async (e: React.MouseEvent) => {
@@ -99,228 +146,208 @@ export const MyProfilePage: React.FC = () => {
   };
 
   return (
-    <div className="content-wrapper">
-      <h2>Your profile</h2>
-      {userData && (
-        <form className="form" onSubmit={userSaveHandler}>
-          <FieldSet legend="User details">
-            <ErrorList details={errors} />
+    <>
+      <div className="content-wrapper">
+        <h2>Your profile</h2>
+        {userData && (
+          <form className="form" onSubmit={userSaveHandler}>
+            <FieldSet legend="User details">
+              <ErrorList details={errors} />
 
-            <FormField htmlFor="profilePageUserName" label="Username">
-              <TextInput
-                id="profilePageUserName"
-                type="text"
-                required
-                placeHolder="username"
-                value={userData?.name}
-                onChange={(e) =>
-                  dispatch(
-                    Actions.setUserData({ ...userData, name: e.target.value })
-                  )
-                }
-              />
-            </FormField>
+              <FormField htmlFor="profilePageUserName" label="Username">
+                <TextInput
+                  id="profilePageUserName"
+                  type="text"
+                  required
+                  placeHolder="username"
+                  value={userData?.name}
+                  onChange={(e) =>
+                    setUserData({ ...userData, name: e.target.value })
+                  }
+                />
+              </FormField>
 
-            <FormField htmlFor="profilePageFullName" label="Full name">
-              <TextInput
-                id="profilePageFullName"
-                type="text"
-                required
-                placeHolder="your full name"
-                value={userData?.fullName}
-                onChange={(e) =>
-                  dispatch(
-                    Actions.setUserData({
+              <FormField htmlFor="profilePageFullName" label="Full name">
+                <TextInput
+                  id="profilePageFullName"
+                  type="text"
+                  required
+                  placeHolder="your full name"
+                  value={userData?.fullName}
+                  onChange={(e) =>
+                    setUserData({
                       ...userData,
                       fullName: e.target.value,
                     })
-                  )
-                }
-              />
-            </FormField>
+                  }
+                />
+              </FormField>
 
-            <FormField htmlFor="profilePageDescription" label="Description">
-              <TextArea
-                rows={5}
-                placeHolder="Describe yourself"
-                value={userData?.description || ''}
-                onChange={(e) =>
-                  dispatch(
-                    Actions.setUserData({
+              <FormField htmlFor="profilePageDescription" label="Description">
+                <TextArea
+                  rows={5}
+                  placeHolder="Describe yourself"
+                  value={userData?.description || ''}
+                  onChange={(e) =>
+                    setUserData({
                       ...userData,
                       description: e.target.value,
                     })
-                  )
-                }
-              />
-            </FormField>
+                  }
+                />
+              </FormField>
 
-            <FormField htmlFor="profilePagePassword" label="Password">
-              <TextInput
-                id="profilePagePassword"
-                type="password"
-                placeHolder="******"
-                value={userData?.password || ''}
-                onChange={(e) =>
-                  dispatch(
-                    Actions.setUserData({
+              <FormField htmlFor="profilePagePassword" label="Password">
+                <TextInput
+                  id="profilePagePassword"
+                  type="password"
+                  placeHolder="******"
+                  value={userData?.password || ''}
+                  onChange={(e) =>
+                    setUserData({
                       ...userData,
                       password: e.target.value,
                     })
-                  )
-                }
-              />
-            </FormField>
+                  }
+                />
+              </FormField>
 
-            <FormField htmlFor="profilePageEmail" label="Email">
-              <TextInput
-                id="profilePageEmail"
-                type="email"
-                required
-                placeHolder="your email address"
-                value={userData?.email || ''}
-                onChange={(e) =>
-                  dispatch(
+              <FormField htmlFor="profilePageEmail" label="Email">
+                <TextInput
+                  id="profilePageEmail"
+                  type="email"
+                  required
+                  placeHolder="your email address"
+                  value={userData?.email || ''}
+                  onChange={(e) =>
                     Actions.setUserData({ ...userData, email: e.target.value })
-                  )
-                }
-              />
-            </FormField>
+                  }
+                />
+              </FormField>
 
-            <FormField htmlFor="profilePageCompany" label="Company">
-              <TextInput
-                id="profilePageCompany"
-                type="text"
-                placeHolder="where you work"
-                value={userData?.company || ''}
-                onChange={(e) =>
-                  dispatch(
-                    Actions.setUserData({
+              <FormField htmlFor="profilePageCompany" label="Company">
+                <TextInput
+                  id="profilePageCompany"
+                  type="text"
+                  placeHolder="where you work"
+                  value={userData?.company || ''}
+                  onChange={(e) =>
+                    setUserData({
                       ...userData,
                       company: e.target.value,
                     })
-                  )
-                }
-              />
-            </FormField>
+                  }
+                />
+              </FormField>
 
-            <FormField htmlFor="profilePageLocation" label="Location">
-              <TextInput
-                id="profilePageLocation"
-                type="text"
-                placeHolder="where you live (eg. Internet)"
-                value={userData?.location || ''}
-                onChange={(e) =>
-                  dispatch(
-                    Actions.setUserData({
+              <FormField htmlFor="profilePageLocation" label="Location">
+                <TextInput
+                  id="profilePageLocation"
+                  type="text"
+                  placeHolder="where you live (eg. Internet)"
+                  value={userData?.location || ''}
+                  onChange={(e) =>
+                    setUserData({
                       ...userData,
                       location: e.target.value,
                     })
-                  )
-                }
-              />
-            </FormField>
+                  }
+                />
+              </FormField>
 
-            <FormField htmlFor="profilePageTwitter" label="Twitter handle">
-              <TextInput
-                id="profilePageTwitter"
-                type="text"
-                value={userData?.twitterHandle || ''}
-                placeHolder="your twitter handle without @"
-                onChange={(e) =>
-                  dispatch(
-                    Actions.setUserData({
+              <FormField htmlFor="profilePageTwitter" label="Twitter handle">
+                <TextInput
+                  id="profilePageTwitter"
+                  type="text"
+                  value={userData?.twitterHandle || ''}
+                  placeHolder="your twitter handle without @"
+                  onChange={(e) =>
+                    setUserData({
                       ...userData,
                       twitterHandle: e.target.value,
                     } as User)
-                  )
-                }
-              />
-            </FormField>
+                  }
+                />
+              </FormField>
 
-            <FormField htmlFor="profilePageGithub" label="GitHub user">
-              <TextInput
-                id="profilePageGithub"
-                type="text"
-                placeHolder="your github user"
-                value={userData?.githubUser || ''}
-                onChange={(e) =>
-                  dispatch(
-                    Actions.setUserData({
+              <FormField htmlFor="profilePageGithub" label="GitHub user">
+                <TextInput
+                  id="profilePageGithub"
+                  type="text"
+                  placeHolder="your github user"
+                  value={userData?.githubUser || ''}
+                  onChange={(e) =>
+                    setUserData({
                       ...userData,
                       githubUser: e.target.value,
                     })
-                  )
-                }
-              />
-            </FormField>
+                  }
+                />
+              </FormField>
 
-            <FormField htmlFor="profilePageHomepage" label="Website">
-              <TextInput
-                id="profilePageHomepage"
-                type="text"
-                placeHolder="eg. link to your portfolio website"
-                value={userData?.homepage || ''}
-                onChange={(e) =>
-                  dispatch(
-                    Actions.setUserData({
+              <FormField htmlFor="profilePageHomepage" label="Website">
+                <TextInput
+                  id="profilePageHomepage"
+                  type="text"
+                  placeHolder="eg. link to your portfolio website"
+                  value={userData?.homepage || ''}
+                  onChange={(e) =>
+                    setUserData({
                       ...userData,
                       homepage: e.target.value,
                     })
-                  )
-                }
-              />
-            </FormField>
+                  }
+                />
+              </FormField>
 
-            <FormField htmlFor="profilePagePronouns" label="Pronouns">
-              <TextInput
-                id="profilePagePronouns"
-                type="text"
-                placeHolder="your pronouns"
-                value={userData?.pronouns || ''}
-                onChange={(e) =>
-                  dispatch(
-                    Actions.setUserData({
+              <FormField htmlFor="profilePagePronouns" label="Pronouns">
+                <TextInput
+                  id="profilePagePronouns"
+                  type="text"
+                  placeHolder="your pronouns"
+                  value={userData?.pronouns || ''}
+                  onChange={(e) =>
+                    setUserData({
                       ...userData,
                       pronouns: e.target.value,
                     })
-                  )
-                }
-              />
-            </FormField>
+                  }
+                />
+              </FormField>
 
-            <div className="form__buttons">
-              <Button type={ButtonType.Submit}> save </Button>
-            </div>
-          </FieldSet>
-
-          <FieldSet legend="Account management">
-            <div className="form">
-              <div className="button-group">
-                <Button type={ButtonType.Button} onClick={logoutHandler}>
-                  logout
-                </Button>
-
-                <Button
-                  type={ButtonType.Button}
-                  kind={ButtonKind.Secondary}
-                  onClick={downloadHandler}
-                >
-                  download my data
-                </Button>
-
-                <Button
-                  type={ButtonType.Button}
-                  kind={ButtonKind.Danger}
-                  onClick={deleteHandler}
-                >
-                  delete my account
-                </Button>
+              <div className="form__buttons">
+                <Button type={ButtonType.Submit}> save </Button>
               </div>
-            </div>
-          </FieldSet>
-        </form>
-      )}
-    </div>
+            </FieldSet>
+
+            <FieldSet legend="Account management">
+              <div className="form">
+                <div className="button-group">
+                  <Button type={ButtonType.Button} onClick={logoutHandler}>
+                    logout
+                  </Button>
+
+                  <Button
+                    type={ButtonType.Button}
+                    kind={ButtonKind.Secondary}
+                    onClick={downloadHandler}
+                  >
+                    download my data
+                  </Button>
+
+                  <Button
+                    type={ButtonType.Button}
+                    kind={ButtonKind.Danger}
+                    onClick={deleteHandler}
+                  >
+                    delete my account
+                  </Button>
+                </div>
+              </div>
+            </FieldSet>
+          </form>
+        )}
+      </div>
+    </>
   );
 };
