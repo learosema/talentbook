6521ff1c8513cfa277c349a8e4c682cd24f3193d
Lea Rosema <lea@lea.codes>
2022-03-23 11:27:02 +0100

    fix: add react-query devtools

--- packages/frontend/src/index.tsx
+++ packages/frontend/src/index.tsx
@@ -1,5 +1,6 @@
 import ReactDOM from 'react-dom';
 import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
 import './css/index.scss';
 
 import './polyfills';
@@ -10,6 +11,7 @@ const queryClient = new QueryClient();
 import { AppProvider } from './store/app.context';
 ReactDOM.render(
   <QueryClientProvider client={queryClient}>
+    <ReactQueryDevtools initialIsOpen={false} />
     <AppProvider>
       <App />
     </AppProvider>
