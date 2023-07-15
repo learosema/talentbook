04866f62f14d11b5b512252b3f0e3f091bf3b3e7
Lea Rosema <lea@lea.codes>
2023-07-15 21:49:09 +0200

    chore: replace skill table with cards

--- packages/frontend/src/components/skill-table/skill-table.test.tsx
+++
@@ -1,9 +0,0 @@
-import { SkillTable } from './skill-table';
-import { createRoot } from 'react-dom/client';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  const root = createRoot(div)
-  root.render(<SkillTable editMode={false} />);
-  root.unmount();
-});
